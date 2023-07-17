acf071425182fe141b5afe809dd043b283e10284
Sebastien Ringrose <seb@codeworks.me>
2023-07-17 09:15:07 +0000

    fix: module ext

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import * as Peko from "https://deno.land/x/peko@1.9.0/mod.ts";
-import { getAccess, POST2Sheet } from "./utils/gcp";
+import { getAccess, POST2Sheet } from "./utils/gcp.ts";
 
 const server = new Peko.Server();
 server.use(Peko.logger(console.log));
