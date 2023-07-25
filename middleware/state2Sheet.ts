9a35e5c18334aa18d7ca3df12b006e0a49262bfb
Sebastien Ringrose <seb@codeworks.me>
2023-07-25 17:19:46 +0100

    fix: state2Sheet middleware

--- middleware/state2Sheet.ts
+++ middleware/state2Sheet.ts
@@ -1,20 +1,22 @@
+import { Middleware } from "https://deno.land/x/peko@1.9.0/mod.ts";
 import { getAccess, POST2Sheet } from "../utils/gcp.ts";
 import { system } from "../models/System.ts";
-import { State } from "../types.ts";
+import { Device } from "../types.ts";
 
 const kv = await Deno.openKv(); 
 let waiting = false
 
 // quick hacky middleware to get data POSTed once per set of requests
-const state2Sheet = async (ctx, next) => {
+export const state2Sheet: Middleware = async (ctx, next) => {
   next()
   if (ctx.request.url.includes("sense") && !waiting) {
     waiting = true
     await getAccess()
-    await new Promise(res => setTimeout(res, 500))
-    const states = await Promise.all(system.devices.map(async device => {
-      return (await kv.get(["state", device.id])).value as State
-    }))
+    await new Promise(res => setTimeout(res, system.polling_interval/2))
+    const states: Record<string, Device> = {}
+    for (const device_id in system.devices) {
+      states[device_id] = (await kv.get(["state", device_id])).value as Device
+    }
     POST2Sheet(states)
     waiting = false
   }
